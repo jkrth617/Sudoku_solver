824ff4c9ebe16e02abbc99f7f962a3e1f990d7ec
jkrth617 <jkrth617@gmail.com>
2015-11-12 13:45:08 -0500

    added the funtionality to fill and clear form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,42 @@
 $(document).on('ready', function () {
   $('#solve').on('click', function (e) {
     var myData = $('form').serialize();
-    debugger;
+    var boardString = getValue(myData);
+    var sudokuGame = new Sudoku(boardString);
+    var answerArray = sudokuGame.solveGame();
+    if (answerArray) {
+      $('#error').hide();
+      fillInBoard(answerArray);
+    }else {
+      $('#error').show();
+    }
   })
+
+  $('#clear').on('click', clearForm)
 })
 
-var getValue = function (bigString) {
+var validInputs = _.map(_.range(1, 10), function (num) {return num+""} )
 
+var getValue = function (bigString) {
+  var valueArray = bigString.split("&");
+  valueArray = _.map(valueArray, function (input) {
+    var breakIndex = input.indexOf("=");
+    var cell = input.substring(breakIndex+1);
+    if (cell === "" || !_.contains(validInputs, cell)) {cell = "-";}
+   return cell;
+  })
+  return valueArray.join("")
 };
+
+var fillInBoard = function (answer) {
+  for(var i = 0; i < answer.length; i++){
+    $('#cell'+ answer[i].index).val(answer[i].value);
+  }
+};
+
+var clearForm = function () {
+  for(var i = 0; i < 81; i++){
+    $('#cell'+i).val("");
+  }
+};
+
